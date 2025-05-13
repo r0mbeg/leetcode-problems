3e798e633b2ce3bd66df756bbd41711d0386296f
Roman <roma564888@yandex.ru>
2025-05-13 21:08:44 +0300

    Top K Frequent Elements

--- src/main/java/ru/proffen/TopKFrequent.java
+++ src/main/java/ru/proffen/TopKFrequent.java
@@ -1,36 +1,36 @@
 package ru.proffen;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 //Leetcode 347. Top K Frequent Elements
 public class TopKFrequent {
     public static int[] topKFrequent(int[] nums, int k) {
 
-        Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
+        // Считаем количество повторений
         for (int n : nums) {
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
 
-        System.out.println(map);
+        List<Map.Entry<Integer, Integer>> entries = new ArrayList<>(map.entrySet());
 
-        int[] res = map.entrySet()
-                .stream()
-                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-                .limit(k)
-                .map(Map.Entry::getKey)
-                .mapToInt(Integer::intValue) // превращает Stream<Integer> в IntStream
-                .toArray();
+        entries.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
 
-        System.out.println(Arrays.toString(res));
+
+        int[] res = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            res[i] = entries.get(i).getKey();
+        }
+
         return res;
     }
 
     public static void main(String[] args) {
+
         topKFrequent(new int[]{-1, -1, 1, 2, 2, 3, 4}, 2);
     }
 
