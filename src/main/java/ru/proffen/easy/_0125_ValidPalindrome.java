fd2f16c898683f0ddf27548c7835ba83116ac46f
Roman <roma564888@yandex.ru>
2025-08-06 15:49:01 +0300

    feat: solution _0125_ValidPalindrome.java (fast)

--- src/main/java/ru/proffen/easy/_0125_ValidPalindrome.java
+++ src/main/java/ru/proffen/easy/_0125_ValidPalindrome.java
@@ -2,20 +2,33 @@ package ru.proffen.easy;
 
 public class _0125_ValidPalindrome {
     public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+
         int n = s.length();
+        /*
+
+
+        System.out.println(s + " : " + n);
 
         return switch (n) {
-            case 1 -> true;
+            case 0, 1 -> true;
             case 2 -> s.charAt(0) == s.charAt(1);
             case 3 -> s.charAt(0) == s.charAt(2);
-            default -> s.charAt(0) == s.charAt(n - 1) && isPalindrome(s.substring(1, n - 2));
-        };
+            default -> s.charAt(0) == s.charAt(n - 1) && isPalindrome(s.substring(1, n - 1));
+        };*/
 
+
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-
     public static void main(String[] args) {
-        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("0P"));
     }
 }
 
